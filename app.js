65ddff42f344da867966cb20ff4355cdb37e0be3
Paul Pichon F <paul10_barca@hotmail.com>
2023-01-25 19:13:06 -0600

    Creacion del menu de la aplicacion

--- app.js
+++ app.js
@@ -9,9 +9,22 @@ console.clear();
 //funcion asincrona
 const main = async() => {
 
-    console.log('Hola Mundo');
+    //definimos opt como un string vacio
+    let opt = '';
 
-    mostrarMenu();
+    do {
+        
+        //al tener promesas en mensajes.js
+        //podemos hacer uso de await
+        //lo que le estamos diciendo es que mostrarMenu() debe esperar hastaque tenga una resolucion
+        opt = await mostrarMenu();
+        console.log( {opt} );
+        //salir del menu de la aplicacion
+        if( opt !== '0') await pausa();
+        
+
+    } while ( opt !== '0');//mientras opt sea diferente de 0 el menu se repetira indefinidamente
+
 
     // pausa();
 
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -4,57 +4,76 @@ require('colors');
 //funcion para mostrar menu
 const mostrarMenu = () => {
 
-    //limpiar consola
-    console.clear();
+    //resolveremos con un new Promise()
+    return new Promise( resolve => {
+        
+        //limpiar consola
+        console.clear();
 
-    //construir interfaz del menu
-    console.log('================================'.green);
-    console.log('     Selecciona una opción      '.green);
-    console.log('================================\n'.green);
+        //construir interfaz del menu
+        console.log('================================'.green);
+        console.log('     Selecciona una opción      '.green);
+        console.log('================================\n'.green);
 
-    console.log(`${'1.'.green} Crear Lista`);
-    console.log(`${'2.'.green} Listar tareas`);
-    console.log(`${'3.'.green} Listar tareas completadas`);
-    console.log(`${'4.'.green} Listar tareas pendientes`);
-    console.log(`${'5.'.green} Completar tarea(s)`);
-    console.log(`${'6.'.green} Borrar tarea`);
-    console.log(`${'0.'.green} Salir \n`);
+        console.log(`${'1.'.green} Crear Lista`);
+        console.log(`${'2.'.green} Listar tareas`);
+        console.log(`${'3.'.green} Listar tareas completadas`);
+        console.log(`${'4.'.green} Listar tareas pendientes`);
+        console.log(`${'5.'.green} Completar tarea(s)`);
+        console.log(`${'6.'.green} Borrar tarea`);
+        console.log(`${'0.'.green} Salir \n`);
 
-    //recibir informacion del usuario
-    //preparar la interfaz para el usuario con readline
-    //esta interfas es para recibir y mandar informacion al usuario
-    const readline = require('readline').createInterface({
-        //para esperar cierta informacion por parte del usuario
-        input: process.stdin,
-        //para mostrar cierto mensjae al usuario cuando esta interactuando
-        output: process.stdout
+        //recibir informacion del usuario
+        //preparar la interfaz para el usuario con readline
+        //esta interfas es para recibir y mandar informacion al usuario
+        const readline = require('readline').createInterface({
+            //para esperar cierta informacion por parte del usuario
+            input: process.stdin,
+            //para mostrar cierto mensjae al usuario cuando esta interactuando
+            output: process.stdout
+        });
+
+        readline.question('Seleccione una opción: ', (opt) => {
+            // console.log({ opt });
+            //cerrar el readline de lo contrario se va aquedar esperando informacion
+            readline.close();
+            //mandar opcion seleccionada
+            resolve( opt );
+        });
+
     });
 
-    readline.question('Seleccione una opción: ', (opt) => {
-        // console.log({ opt });
-        //cerrar el readline de lo contrario se va aquedar esperando informacion
-        readline.close();
-    });
+    
 
 
 }
 
+
+
 //pausa
 const pausa = () => {
-    //recibir informacion del usuario
-    //preparar la interfaz para el usuario con readline
-    //esta interfas es para recibir y mandar informacion al usuario
-    const readline = require('readline').createInterface({
-        //para esperar cierta informacion por parte del usuario
-        input: process.stdin,
-        //para mostrar cierto mensjae al usuario cuando esta interactuando
-        output: process.stdout
+
+    //new Promise()
+    return new Promise( resolve => {
+        //recibir informacion del usuario
+        //preparar la interfaz para el usuario con readline
+        //esta interfas es para recibir y mandar informacion al usuario
+        const readline = require('readline').createInterface({
+            //para esperar cierta informacion por parte del usuario
+            input: process.stdin,
+            //para mostrar cierto mensjae al usuario cuando esta interactuando
+            output: process.stdout
+        });
+
+        readline.question(`\nPresione ${'ENTER'.green} para continuar\n`, (opt) => {
+            //cerrar el readline de lo contrario se va aquedar esperando informacion
+            readline.close();
+            //llamamos el resolve para resolver la promesa
+            //no se pone nada dentro del resolve() porque no interesa nada de lo que escriba el usuario
+            resolve();
+        });    
     });
 
-    readline.question(`\nPresione ${'ENTER'.green} para continuar\n`, (opt) => {
-        //cerrar el readline de lo contrario se va aquedar esperando informacion
-        readline.close();
-    });
 }
 
 
