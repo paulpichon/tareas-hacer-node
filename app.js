f4d3cfdda43b1edc65acbdfdee55ebe3d74b9399
Paul Pichon F <paul10_barca@hotmail.com>
2023-01-25 18:07:12 -0600

    Inicio de proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+//colores en la consola
+require('colors');
+//limpiar la consola
+console.clear();
+
+//funcion asincrona
+const main = async() => {
+
+    console.log('Hola Mundo');
+}
+
+//llamar el main
+main();
